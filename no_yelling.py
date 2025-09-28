b99352ad4009172ebae653fde00b73466f4a8bd4
DamianOakley <damianoakley3@gmail.com>
2025-09-28 05:37:43 -0400

    Add files via upload

---
+++ no_yelling.py
@@ -0,0 +1,22 @@
+#No yelling!
+#https://www.codewars.com/kata/587a75dbcaf9670c32000292
+
+def no_yelling(text:str):
+    word = text.split()
+    no_space = " ".join(word).lower()
+
+    sentences = no_space.capitalize() 
+
+    proper_sentence = "".join(sentences.split(text))
+    
+    return proper_sentence
+
+print(no_yelling("You       can HAVE some more honey."))
+print(no_yelling("how are you DoIng toDAY?"))
+print(no_yelling("I like to eat ICe     cream!"))
+print(no_yelling("BOBBY shouldn't BE   ALLOWED     to   voTE."))
+
+#This function ensures that every sentence is properly formatted in correct sentence structure.
+#For each string, the function removes any unneccesary spacing, while lowercasing every word.
+#The sentence is the capitallized, and each word is evenly spaced out.
+#Finally, the correct sentence is returned to the user.
